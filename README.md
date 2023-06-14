438dfecc98375965905b86c89b4e2ae4e98dc132
Martín <magadiflo@gmail.com>
2023-06-13 22:31:54 -0500

    RouterLink y RouterLinkOptions

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.1.
 
+## routerLinkActiveOptions
+
+Para agregar las clases solo cuando la URL coincida exactamente con el enlace, agregue la opción **exact: true**
+
+```html
+<a href="#" [routerLink]="['']" class="list-group-item list-group-item-action" routerLinkActive="active"
+  [routerLinkActiveOptions]="{ exact: true }" aria-current="true">
+  Home page
+</a>
+```
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/components/contact-page/contact-page.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent, },
+  { path: '', component: HomePageComponent, },
   { path: 'about', component: AboutPageComponent, },
-  { path: '**', redirectTo: 'home', }
+  { path: 'contact', component: ContactPageComponent, },
+  { path: '**', redirectTo: '', }
 ];
 
 @NgModule({
